40bf6301661c11bdcea970ceca2894d2dd9d8d6f
s742 <s742.sup32n0v4@gmail.com>
2025-08-18 15:15:19 +0700

    config enviroment to setting

--- init.lua
+++ init.lua
@@ -1,17 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",
-      lazypath
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("keymaps")
-require("options")
-
-require("lazy").setup("plugins")
+require("s742.core.options")
+require("s742.core.keymaps")
+require("s742.core.colorscheme")
+--require("s742.core.lazy")
